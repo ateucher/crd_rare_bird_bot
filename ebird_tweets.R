960653168679d9dd2955525a75a0a11f692de9ff
ateucher <Andy.Teucher@gmail.com>
2014-09-30 23:00:40 -0700

    Compare to old_tweets so don't repeat.

--- ebird_tweets.R
+++ ebird_tweets.R
@@ -2,13 +2,14 @@ library("rebird")
 library("dplyr")
 library("httr")
 library("twitteR")
+library("methods") # Required for use with RScript
 
-source("keys.R")
-source("fun.R")
+source("/Users/ateucher/dev/crd_rare_bird_bot/keys.R")
+source("/Users/ateucher/dev/crd_rare_bird_bot/fun.R")
 
 crd <- ebirdnotable(region = "CA-BC-CP", regtype = "subnational2", back = 1, 
                     provisional = TRUE, hotspot = FALSE, simple = FALSE)
-
+ 
 if (nrow(crd) > 0) {
   crd <- crd %>%
     mutate(url = paste0("http://ebird.org/ebird/view/checklist?subID=", subID), 
@@ -20,18 +21,23 @@ if (nrow(crd) > 0) {
                              short_url))
   
   ## Don't duplicate
-  load("old_tweets.rda")
-  tweets  <- tweets[!tweets %in% old_tweets]
+  if (file.exists("/Users/ateucher/dev/crd_rare_bird_bot/old_tweets.rda")) {
+    load("/Users/ateucher/dev/crd_rare_bird_bot/old_tweets.rda")
+    tweets  <- tweets[!tweets %in% old_tweets]
+    old_tweets <- c(old_tweets, tweets)
+  } else {
+    old_tweets <- tweets
+  }
   
-  # Twitter
+  ## Twitter
   
   setup_twitter_oauth(crd_tw_api_key, crd_tw_api_key_secret, crd_tw_access_token, 
                       crd_tw_access_token_secret)
   
   lapply(tweets, tweet)
   
-  old_tweets <- c(old_tweets, tweets)
   
-  save(old_tweets, file = "old_tweets.rda")
+  
+  save(old_tweets, file = "/Users/ateucher/dev/crd_rare_bird_bot/old_tweets.rda")
   
 }
