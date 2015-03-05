10d224c413000a0bfd8da5dc8c2e2c7696a1ff76
ateucher <Andy.Teucher@gmail.com>
2015-03-04 23:14:20 -0800

    Move processing freqs into main file

--- ebird_tweets.R
+++ ebird_tweets.R
@@ -9,8 +9,30 @@ source("/Users/ateucher/dev/crd_rare_bird_bot/keys.R")
 source("/Users/ateucher/dev/crd_rare_bird_bot/fun.R")
 
 ## Use process_freq_hist here instead of this call:
-crd <- ebirdnotable(region = "CA-BC-CP", regtype = "subnational2", back = 3, 
-                    provisional = TRUE, hotspot = FALSE, simple = FALSE)
+freq <- get_freq("counties", "CA-BC-CP", 1990, 2014, 1, 12)
+
+# Get a list of common birds for the current quarter of the month
+common_now <- get_common(freq, Sys.Date(), 0.01)
+
+# Get all the bird sightings for the region
+crd_birds <- ebirdregion(region = "CA-BC-CP", regtype = "subnational2", back = 3, 
+                         provisional = TRUE, hotspot = FALSE)
+
+# Get the rare birds out of all the sightings and extract their locations
+rare_birds <- crd_birds[!crd_birds$comName %in% common_now,]
+locs <- unique(rare_birds$locID)
+
+# Get full checklists for those locations
+loc_df <- get_full_checklists(locs)
+
+# Extract the rare birds from those locations
+rare_birds <- loc_df[!loc_df$comName %in% common_now,]
+
+# Get tagged rare birds
+crd_rare <- ebirdnotable(region = "CA-BC-CP", regtype = "subnational2", back = 3, 
+                         provisional = TRUE, hotspot = FALSE, simple = FALSE)
+
+crd <- unique(rbind(rare_birds, crd_rare))
  
 if (nrow(crd) > 0) {
   tweets <- make_tweets(crd, BitlyToken)
@@ -18,7 +40,7 @@ if (nrow(crd) > 0) {
   ## Don't duplicate
   if (file.exists("/Users/ateucher/dev/crd_rare_bird_bot/old_tweets.rda")) {
     load("/Users/ateucher/dev/crd_rare_bird_bot/old_tweets.rda")
-    tweets  <- tweets[!tweets %in% old_tweets]
+    tweets  <- setdiff(tweets, old_tweets)
     old_tweets <- c(old_tweets, tweets)
   } else {
     old_tweets <- tweets
