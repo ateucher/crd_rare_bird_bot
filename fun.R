44c9aa58c3458d0a67722885c3323021493f754e
ateucher <Andy.Teucher@gmail.com>
2015-06-04 06:59:44 -0700

    Use vapply instead of sapply

--- fun.R
+++ fun.R
@@ -38,7 +38,8 @@ get_freq <- function(loctype, loc, startyear, endyear, startmonth, endmonth) {
   asChar <- readBin(ret$content, "character")
   freq <- read.delim(text = asChar, skip = 12, 
                      stringsAsFactors = FALSE)[-1,-50]
-  names(freq) <- c("Species", sapply(month.name, paste ,1:4, sep="-"))
+  names(freq) <- c("Species", vapply(month.name, paste, FUN.VALUE = character(4), 
+                                     1:4, sep="-"))
   freq_long <- tidyr::gather(freq, "mo_qt", "Freq", 2:length(freq), 
                              convert = TRUE)
   freq_long
@@ -76,8 +77,8 @@ make_tweets <- function(bird_df, bitly_token) {
   bird_df <- mutate(bird_df, 
                     url = paste0("http://ebird.org/ebird/view/checklist?subID=", 
                                  subID), 
-                    short_url = sapply(url, shorten, token = bitly_token, 
-                                       USE.NAMES = FALSE))
+                    short_url = vapply(url, shorten, FUN.VALUE = character(1), 
+                                       token = bitly_token, USE.NAMES = FALSE))
   bird_df <- arrange(bird_df, obsDt)
   bird_df$conf <- with(bird_df, 
                        ifelse(obsReviewed & obsValid, " (CONFIRMED). ", 
